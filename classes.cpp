71b5e465587a909c9d126be5f20b09d1ea1d2adf
Peter Kwiatek <pkwiatek6@gmail.com>
2016-10-11 16:41:39 -0400

    Added functions to main and what they do

--- classes.cpp
+++ classes.cpp
@@ -16,16 +16,20 @@ int RPS::winCompare(int player, int ai){
     return 0;
   }
 }
+//meant to always lose against the player
 int RPS::aiEasy(int player){
   return ((player % numChoices) - 1);
 }
+//meant to be the most fair to play against
 int RPS::aiMedium(){
   srand(time(null));
   return rand() % numChoices + 1;
 }
+// should play againt the average
 int RPS::aiHard(int player){
 
 }
+//always meant to win
 int RPS::aiImpossible(intPlayer){
   return ((player % numChoices) + 1);
 }
--- main.cpp
+++ main.cpp
@@ -15,13 +15,65 @@
 //computer AI for hard dificulty should take the average of the player rounded
 #include <iostream>
 using namespace std;
-void Menu(int type);
-int main() {
 
+void Menu(int type);
+bool selectOption(int type);
+
+int main() {
+  do{
+  Menu(0);
+}while(!selectOption(0));
   return 0;
 }
 void Menu(int type){
-  if(type = 0){
-
+  if(type == 0){
+    cout << "***********************" << endl;
+    cout << "Main menu"<< endl;
+    cout << "***********************" << endl;
+    cout << "1. Play" << endl;
+    cout << "2. Difficulty" << endl;
+    cout << "3. Quit" << endl;
+  }else if(type == 1){
+    cout << "***********************" << endl;
+    cout << "Difficulty"<< endl;
+    cout << "***********************" << endl;
+    cout << "1. Easy" << endl;
+    cout << "2. Medium" << endl;
+    cout << "3. Hard" << endl;
+    cout << "4. Impossible" << endl;
+    cout << "5. Back" << endl;
+  }
+}
+bool selectOption(int type){
+  int input = 0;
+  cin << input;
+  if(type == 0){
+    if(input == 1){
+      //play the game with the selected difficulty
+      return true;
+    }else if(input == 2){
+      Menu(1);
+      selectOption(1);
+      //change AI difficulty
+      return true;
+    }else{
+      return false;
+    }
+  }else if(type == 1){
+    if(input == 1){
+      //ai = easy
+      return true;
+    }else if(input == 2){
+      //ai = medium
+      return true;
+    }else if(input == 3){
+      //ai = hard
+      return true;
+    }else if(input == 4){
+      //ai = impossible
+      return true;
+    }else{
+      return false;
+    }
   }
 }
